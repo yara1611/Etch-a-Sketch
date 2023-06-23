0601d457ecc80cd2ec3bcbd033e9d5b47c944d89
yara <76455290+yara1611@users.noreply.github.com>
2023-06-23 03:50:31 +0300

    rework

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ function makeRows(n,y){
         container.innerHTML+=`<div id='${i}'></div>`
     }
 }
-makeRows(16,16)
+
 const divs = document.querySelectorAll('div')
 divs.forEach(div => {
     div.addEventListener('click',(e)=>{
    const d = document.getElementById(e.target.id)
-   d.style.background='red'
+   d.style.background='black'
 })
 div.addEventListener('dblclick',(e)=>{
     const d = document.getElementById(e.target.id)
@@ -18,5 +18,13 @@ div.addEventListener('dblclick',(e)=>{
  })
 });
 
+container.addEventListener('click',(e)=>{
+    const d = document.getElementById('container')
+    d.style.background='white'})
 
+
+const number= prompt('Specifiy the size of grid in squares:')    
+container.style.gridTemplateColumns=  `repeat(${number},100px)`
+container.style.gridTemplateRows=  `minmax(120px,auto) repeat(${number},100px);`    
+makeRows(number,number)
 
